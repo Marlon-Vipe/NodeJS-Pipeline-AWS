f494d55a934a4d7181f4a23c1b5804b525feb3f0
Marlon Villalona <iamvillalona@gmail.com>
2021-07-04 10:34:31 -0400

    First pipeline

--- Nodejs-pipeline/index.js
+++ Nodejs-pipeline/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Construyendo PipeLine con Heroku!\n'
+  const msg = 'My first pipeline in Heroku!\n'
   res.end(msg);
 });
 
